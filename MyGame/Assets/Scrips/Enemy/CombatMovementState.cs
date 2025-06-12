92ae6945422c79350a191171cba1b66b1181f1f6
givuj <2920152382@qq.com>
2025-06-12 16:52:16 +0800

    增加在主机周围旋转

--- MyGame/Assets/Scrips/Enemy/CombatMovementState.cs
+++ MyGame/Assets/Scrips/Enemy/CombatMovementState.cs
@@ -1,14 +1,24 @@
+using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+
 public enum AICombatState {Idle,Chase,Circling }
 public class CombatMovementState : State<EnemyController>
 {
     // Start is called before the first frame update
     EnemyController enemy;
+    [SerializeField] float circlingSpeed = 20f;
     [SerializeField] float  distanceToStand = 3f;
+    [SerializeField] Vector2 idleTimeRange = new Vector2(2, 5);
+    [SerializeField] Vector2 circlingTimeRange = new Vector2(3, 6);
     [SerializeField] float  adjust = 1f;//调整的距离
     AICombatState state;
+    float timer = 0f;
+    int circlingDir = 1;
     public override void Enter(EnemyController owner)
     {
         enemy = owner;
@@ -25,8 +35,17 @@ public class CombatMovementState : State<EnemyController>
         }
         if (state == AICombatState.Idle)
         {
-
-
+            if (timer <= 0)
+            {
+                if (UnityEngine.Random.Range(0, 2) == 0)
+                {
+                    StartIdle();
+                }
+                else
+                {
+                    StartCircling();
+                }
+            }
         }
         else if(state == AICombatState.Chase )
         {
@@ -42,9 +61,17 @@ public class CombatMovementState : State<EnemyController>
         }
         else if(state == AICombatState.Circling)
         {
-
+            if(timer<=0)
+            {
+                StartIdle();
+                return;
+            }
+            transform.RotateAround(enemy.Target.transform.position, Vector3.up, circlingSpeed * circlingDir*Time.deltaTime);
         }
-        
+        if(timer>0)
+        {
+            timer -= Time.deltaTime;//计数器减去每帧经过的时间
+        }
         
     }
     void StartChase()
@@ -52,13 +79,26 @@ public class CombatMovementState : State<EnemyController>
         Debug.Log(5);
         state = AICombatState.Chase;
         enemy.Animator.SetBool("CombatMode",false);
+        enemy.Animator.SetBool("circling", false);
     }
     void StartIdle()
     {
         state = AICombatState.Idle;
+        timer = UnityEngine.Random.Range(idleTimeRange.x,idleTimeRange.y);
         enemy.Animator.SetBool("CombatMode", true);
-    }
+        enemy.Animator.SetBool("circling", false);
 
+    }
+    void StartCircling()
+    {
+
+        state = AICombatState.Circling;
+        timer = Random.Range(circlingTimeRange.x, circlingTimeRange.y);
+        circlingDir = Random.Range(0, 2) == 0 ? 1 : -1;
+        enemy.Animator.SetBool("circling",true);    
+        enemy.Animator.SetFloat("circlingDir", circlingDir);    
+
+    }
     public override void Exit()
     {
         Debug.Log("Exit chase");
